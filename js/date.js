72b83018bee64cafbcc87858690598fb1170b94d
Prateek Singh Arora <psarora@sfu.ca>
2024-02-09 17:19:08 -0800

    index v2 created, timeline moved out of hero

--- js/date.js
+++ js/date.js
@@ -2,11 +2,18 @@
 function displayDate() {
     var date = new Date();
     var day = date.getDate();
+    
     var monthNames = ["January", "February", "March", "April", "May", "June",
         "July", "August", "September", "October", "November", "December"
     ];
+    var suffixNames = ["st", "nd", "rd"];
+
     var month = monthNames[date.getMonth()];
+    var suffix = "th";
+    if (day%10 > 0 && day%10 < 4 && day != 11 && day != 12 && day != 13){
+        suffix = suffixNames[day%10 - 1];
+    }
     var year = date.getFullYear();
     
-    document.getElementById("currentDate").innerHTML = day + " " + month + " " + year;
+    document.getElementById("currentDate").innerHTML = day + suffix + " " + month + " " + year;
 }
